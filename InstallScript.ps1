35bd479eefc49f4a65e5eeb3c18f34d8ef6e2c10
Ceredron <roarmj@gmail.com>
2019-08-21 13:13:49 +0200

    Changed to .bat files, added uninstall script

--- InstallScript.ps1
+++
@@ -1,1 +0,0 @@
-. $Env:windir\Microsoft.NET\Framework\v4.0.30319\installutil "$PSScriptRoot\bin\Release\AutoSubsync.exe"
---
+++ Uninstall.bat
@@ -0,0 +1,2 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\installutil -u %0\..\bin\Release\AutoSubsync.exe
+pause
